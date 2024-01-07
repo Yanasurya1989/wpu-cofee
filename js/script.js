fad59acadccfbe0c0f1fcd430b783b2b20616704
YanaWebProgrammer <teukita15@gmail.com>
2024-01-07 15:01:04 +0700

    finishing nav menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// remove toggle menu with click outer hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
