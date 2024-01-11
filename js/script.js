a5dc0f885b8d0f9ba2c722cb6b6ca62d3c06084d
YanaWebProgrammer <teukita15@gmail.com>
2024-01-11 11:23:04 +0700

    Search form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle (lose or appear) active class
+// Toggle (lose or appear) active class for hamburger-menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // when click hamburgermenu
@@ -6,10 +6,27 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// remove toggle menu with click outer hamburger menu
+// Toggle class active for search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// hilangkan component ketika klik diluar area
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  // jika yang di klik bukan icon search dan bukan form search maka hilangkan class active
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
