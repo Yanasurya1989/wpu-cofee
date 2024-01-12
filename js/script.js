2bfad6a5082c17ec3bdf279ce6dc97015382c229
YanaWebProgrammer <teukita15@gmail.com>
2024-01-12 10:54:30 +0700

    shoping cart, search form, product, detil

--- js/script.js
+++ js/script.js
@@ -16,9 +16,17 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// Toggle class active for shoping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // hilangkan component ketika klik diluar area
 const hamburger = document.querySelector("#hamburger-menu");
 const searchButton = document.querySelector("#search-button");
+const shoppingcarthButton = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -29,4 +37,37 @@ document.addEventListener("click", function (e) {
   if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  if (
+    !shoppingcarthButton.contains(e.target) &&
+    !shoppingCart.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
+  }
+
+  // modal box
+  const itemDetailModal = document.querySelector("#item-detail-modal");
+  // const itemDetailButton = document.querySelector(".item-detail-button");
+  const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+  itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+      itemDetailModal.style.display = "flex";
+      e.preventDefault();
+    };
+  });
+
+  // close button modal
+  document.querySelector(".modal .close-icon").onclick = (e) => {
+    itemDetailModal.style.display = "none";
+    e.preventDefault();
+  };
+
+  // click other componen
+  const modal = document.querySelector("#item-detail-modal");
+  window.onclick = (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 });
