b2ed435b9ee40e3485b90eab7fac566123059615
YanaWebProgrammer <teukita15@gmail.com>
2024-01-28 10:27:38 +0700

    finishing

--- src/app.js
+++ src/app.js
@@ -71,6 +71,67 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// Form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  // console.log(objData);
+
+  // const message = formatMessage(objData);
+  // window.open("http://wa.me/6289601353957?text=" + encodeURIComponent(message));
+
+  // minta transaction token menggunakan ajax/fetch
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    window.snap.pay(token);
+    // console.log(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan whatsapp
+// const formatMessage = (obj) => {
+//   return `Data Customer:
+//     Nama: ${obj.name}
+//     Email: ${obj.email}
+//     Hand phone: ${obj.phone}
+
+//     Data Pesanan:
+//     ${JSON.parse(obj.items).map(
+//       (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+//     )}
+
+//     TOTAL: ${rupiah(obj.total)}
+//     Terimakasih
+//   `;
+// };
+
 // konversi rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
